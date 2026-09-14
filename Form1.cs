b4a4fba1afce7bba02b9abd96e98d68da53a5e4f
João Paulo Rodrigues <joaopaulorods@gmail.com>
2026-09-14 08:54:31 -0300

    Carga inicial

---
+++ Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SistemaTecPDV
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (SistemaTecPDV.Dados.ConexaoBanco
+                    .TestarConexao(out string msg))
+            {
+                MessageBox.Show(msg, "Teste de Conexão",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show(msg, "Teste de Conexão",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+    }
+}
